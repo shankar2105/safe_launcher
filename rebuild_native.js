22826cf6de859543126d4af78ac7823a77d7540c
Shankar <shankar21mail@gmail.com>
2016-09-23 20:58:18 +0530

    ci test 28

--- rebuild_native.js
+++ rebuild_native.js
@@ -1,44 +1,50 @@
-import os from 'os';
-import childProcess from 'child_process';
-import path from 'path';
-import * as electron from 'electron-prebuilt';
-import * as rebuild from 'electron-rebuild';
+/*
+ This script compiles native modules for Electron.
+ If you don't plan to use native modules you can delete this file.
+ But if you'd like to use them do those steps...
+ 1. Go to main project's directory and run:
+ npm install electron-rebuild --save-dev
+ 2. Then go to app folder:
+ cd app
+ 3. Add to app/package.json lines:
+ "scripts": {
+ "postinstall": "node ../tasks/rebuild_native"
+ }
+ 4. Now install your native module:
+ npm install my_native_module --save
+ 5. Trigger once again full npm install so electron-rebuild can do it's job:
+ npm install
+ 6. Enjoy your native module!
+ */
 
-const pathToElectronNativeModules = path.join(__dirname, '/node_modules');
-const electronVersion = require('electron-prebuilt/package.json').version;
+'use strict';
 
-const rebuildNativeModules = () => {
-  rebuild.shouldRebuildNativeModules(electron)
-    .then(function(shouldBuild) {
-      if (!shouldBuild) {
-        return true;
-      }
+var childProcess = require('child_process');
+var path = require('path');
+var Q = require('q');
+var electron = require('electron');
+var electronPackage = require('electron/package.json');
+var rebuild = require('electron-rebuild');
 
-      return rebuild.installNodeHeaders(electronVersion)
-        .then(function() {
-          return rebuild.rebuildNativeModules(electronVersion, pathToElectronNativeModules);
-        });
-    })
-    .then(function() {
-      console.log('Rebuilding complete.');
-    })
-    .catch(function(err) {
-      console.error("Rebuilding error!");
-      console.error(err);
-    });
-};
+var pathToElectronNativeModules = path.join(__dirname, 'node_modules');
 
-const rebuildForWindows = () => {
-  const mods = ['ffi', 'ref'];
-  for (let mod of mods) {
-    console.log('Rebuilding ', mod);
-    childProcess.execSync('node-gyp rebuild --target=' + electronVersion + ' --arch=' + os.arch() +
-      ' --dist-url=https://atom.io/download/atom-shell', {
-      cwd: 'node_modules/' + mod
-    });
-  }
-  console.log('Rebuilding complete.');
-};
+rebuild.shouldRebuildNativeModules(electron)
+  .then(function(shouldBuild) {
+    if (!shouldBuild) {
+      return true;
+    }
 
-console.log('Rebuilding native modules');
-"win32" === os.platform() ? rebuildForWindows() : rebuildNativeModules();
+    console.log('Rebuilding native modules for Electron...');
+
+    return rebuild.installNodeHeaders(electronPackage.version)
+      .then(function() {
+        return rebuild.rebuildNativeModules(electronPackage.version, pathToElectronNativeModules);
+      });
+  })
+  .then(function() {
+    console.log('Rebuilding complete.');
+  })
+  .catch(function(err) {
+    console.error("Rebuilding error!");
+    console.error(err);
+  });
